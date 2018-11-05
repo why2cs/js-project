167332fcc065c2c1b613b045b79a54d7e6cd320d
why2cs <why2cs@outlook.com>
2018-11-05 17:28:33 +0800

    1105

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -13,4 +13,39 @@ function countNodes(){
     alert(body_ele.nodeType)
 }
 
+
+function prepareGallery(){
+    var support=document.getElementById && document.getElementsByTagName
+    if(!support) return false
+    
+    var gallery=document.getElementById("gallery")
+    if(gallery==null) return false
+
+    var links=gallery.getElementsByTagName("a")
+    for(var i=0;i<links.length;i++){
+        links[i].onclick=function(){
+            showPic(this)
+            return false
+        }
+    }
+}
+
+addLoadEvent(prepareGallery)
+
+
+function addLoadEvent(func){
+    var oldEvent=window.onload
+    if(typeof oldEvent != "function"){
+        window.onload=func
+    }else{
+        window.onload=function(){
+            oldEvent()
+            func()
+        }
+    }
+}
+
+
+
+
 //window.onload=countNodes()
